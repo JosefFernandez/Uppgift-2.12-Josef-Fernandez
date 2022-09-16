088350a022df4a2b6a47ef75f072b943c13dab28
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:45:35 +0200

    Add project files.

---
+++ Uppgift 2.12 Josef Fernandez/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_2._12
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int avgift = 300;
+            Console.WriteLine("hur många dagar ska du hyra den här bilen?");
+            int dagar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            int km = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(avgift + dagar * 500 - 500 + km);
+        }
+
+
+    }
+}
